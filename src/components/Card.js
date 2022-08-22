280e87d9eb2a8c65e140afac8d8907a376a0ffb8
NagaveniPusuluri <93608500+NagaveniPusuluri@users.noreply.github.com>
2022-08-22 21:14:11 +0530

    Update Card.js

--- src/components/Card.js
+++ src/components/Card.js
@@ -6,7 +6,7 @@ let img_path="https://image.tmdb.org/t/p/w500";
     return(
         <>
         <div className="movie">
-            <img src={img_path+movie.info.poster_path} className="poster"/>
+            <img src={img_path+movie.info.poster_path} alt="Poster" className="poster"/>
             <div className="movie-details">
                 <div className="box">
                     <h4 className="title">{movie.info.title}</h4>
